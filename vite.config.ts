979a1b30386d5e000e8036b3fcd0494b586eda6f
vish15wa <vishwapatel1535@gmail.com>
2026-02-07 20:17:02 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AssessPro_AI_Hiring_Platform/'
+  base: '/AssessPro_AI_Hiring_Platform/',
 })
